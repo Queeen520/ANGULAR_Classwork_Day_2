41fd446002fdf7191f5e668948899ccaf5798ab5
Isabella W <isabellawindhaber@gmail.com>
2022-10-11 11:31:12 +0200

    -

--- ClassworkDay2/src/app/app.module.ts
+++ ClassworkDay2/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
