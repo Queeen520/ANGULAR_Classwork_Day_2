1d21687b7c5751be52639f0135be2bcc7084702f
Isabella W <isabellawindhaber@gmail.com>
2022-10-11 18:04:28 +0200

    --

--- ClassworkDay2/src/app/main/main.component.ts
+++ ClassworkDay2/src/app/main/main.component.ts
@@ -8,36 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
   cars: Array<{name: string, price: number, img: string}> = [
     { 
-      name: "David", 
-      price: 32,
-      img: ""
+      name: "Mercedes-Benz, SUV", 
+      price: 550000,
+      img: "https://www.frontrunneroutfitters.com/media/catalog/product/cache/11f448c2736cb138681faf0ba59260e1/k/r/krmg009t.1.jpg"
     },
     {
-      name: "jime",
-      price: 28,
-      img: ""
+      name: "Seat Ibiza Xcellence",
+      price: 25000,
+      img: "https://i.pinimg.com/736x/d4/3e/7e/d43e7ec0e89f6507cffc0e506394aa85.jpg"
  
     },
     {
-      name:"sabrina",
-      price: 26,
-      img: ""
+      name:"Jeep Grand Cherokee 2022",
+      price: 160000,
+      img: "https://images.hgmsites.net/hug/2022-jeep-grand-cherokee_100814952_h.jpg"
     },
     { 
-      name: "David", 
-      price: 32,
-      img: ""
+      name: "Porsche Cayenne", 
+      price: 320000,
+      img: "https://cdn.euroncap.com/media/31910/porsche-cayenne-359-235.jpg"
     },
     {
-      name: "jime",
-      price: 28,
-      img: ""
+      name: "Lamborghini Aventador",
+      price: 440000,
+      img: "https://carwow-de-wp-1.imgix.net/lamborghini-aventador-replacement-front-blue-1.jpeg"
  
     },
     {
-      name:"sabrina",
-      price: 26,
-      img: ""
+      name:"Audi Q5",
+      price: 65000,
+      img: "https://storage.carsmile.pl/uploads/2020/12/thumb_audi-q5.jpg"
     }
  ];
   constructor() { }
